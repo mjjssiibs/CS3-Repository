cb4e3af1693bc132cd7f6b11780d496b1408ce78
mjjssiibs <masibal2030@clc.pshs.edu.ph>
2026-08-16 22:54:50 +0800

    Add Chinese Zodiac Sign determination script

---
+++ q1/q1_sg2_a3.py
@@ -0,0 +1,67 @@
+"""SIBAL, Mathea Jasiel A. | ComSci 3
+
+__________________________________________"""
+
+Rat = [1900, 1912, 1924, 1936, 1948, 1960, 1972, 1984, 1996, 2008, 2020]
+Ox = [1901, 1913, 1925, 1937, 1949, 1961, 1973, 1985, 1997, 2009, 2021]
+Tiger = [1902, 1914, 1926, 1938, 1950, 1961, 1974, 1986, 1998, 2010, 2022]
+Rabbit = [1903, 1915, 1927, 1939, 1951, 1963, 1975, 1987, 1999, 2011,2023]
+Dragon = [1904, 1916, 1928, 1940, 1952, 1964, 1976, 1988, 2000, 2012, 2024]
+Snake = [1905, 1917, 1929, 1941, 1953, 1965, 1977, 1989, 2001, 2013, 2025]
+Horse = [1906, 1918, 1930, 1942, 1954, 1966, 1978, 1990, 2002, 2014, 2026]
+Goat = [1907, 1919, 1931, 1943, 1955, 1967, 1979, 1991, 2003, 2015, 2027]
+Monkey = [1908, 1920, 1932, 1944, 1956, 1968, 1980, 1992, 2004, 2016, 2028]
+Rooster = [1909, 1921, 1933, 1945, 1957, 1969, 1981, 1993, 2005, 2017, 2029]
+Dog = [1910, 1922, 1934, 1946, 1958, 1970, 1982, 1994, 2006, 2018, 2030]
+Pig = [1911, 1923, 1935, 1947, 1959, 1971, 1983, 1995, 2007, 2019, 2031 ]
+
+birth_year = int(input("Enter your year of birth (Past 1900): "))
+if birth_year in Rat:
+    print("Your Chinese Zodiac Sign is Rat, or 鼠 / Shǔ in Chinese!")
+    exit()
+        
+elif birth_year in Ox:
+    print("Your Chinese Zodiac Sign is Ox, or 牛 / Niú in Chinese!")
+    exit()
+        
+elif birth_year in Tiger:
+    print("Your Chinese Zodiac Sign is Tiger, or 虎 / Hǔ in Chinese!")
+    exit()
+        
+elif birth_year in Rabbit:
+    print("Your Chinese Zodiac Sign is Rabbit, or 兔 / Tù in Chinese!")
+    exit()
+        
+elif birth_year in Dragon:
+    print("Your Chinese Zodiac Sign is Dragon, or 龙 / Lóng in Chinese!")
+    exit()
+
+elif birth_year in Snake:
+    print("Your Chinese Zodiac Sign is Snake, or 蛇 / Shé in Chinese!")
+    exit()
+        
+elif birth_year in Horse:
+    print("Your Chinese Zodiac Sign is Horse, or 马 / Mǎ in Chinese!")
+    exit()
+        
+elif birth_year in Goat:
+    print("Your Chinese Zodiac Sign is Goat, or 羊 / Yáng in Chinese!")
+    exit()
+        
+elif birth_year in Monkey:
+    print("Your Chinese Zodiac Sign is Monkey, or 猴 / Hóu in Chinese!")
+    exit()
+        
+elif birth_year in Rooster:
+    print("Your Chinese Zodiac Sign is Rooster, or 鸡 / Jī in Chinese!")
+    exit()
+        
+elif birth_year in Dog:
+    print("Your Chinese Zodiac Sign is Dog, or 狗 / Gǒu in Chinese!")
+    exit()
+        
+elif birth_year in Pig:
+    print("Your Chinese Zodiac Sign is Pig, or 猪 / Zhū in Chinese!")
+    exit()
+    
+
